306f4a56f8ea7b3187231fa829e134efff9db099
zakeriaabrar <zakeriaabrar@gmail.com>
2026-04-30 13:35:26 +0600

    implement profile view with note statistics

--- notesbridge/profiles/urls.py
+++ notesbridge/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import  browse_view,dashboard_view
+from .views import  browse_view,dashboard_view,profile_view
 
 urlpatterns = [
     path('dashboard/', dashboard_view, name='dashboard'),
     path('browse/', browse_view, name='browse_notes'),
+    path('my-profile/',profile_view,name='profile'),
 ]
--- notesbridge/profiles/views.py
+++ notesbridge/profiles/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Count
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from departments.models import Department, Subject
 from notes.models import Note
 from comments.models import Comment
+from .forms import ProfileUpdateForm
+from .models import Profile
 from votes.models import Vote
 from reports.models import Report
+from django.db.models import Sum
 from django.core.paginator import Paginator
-
 
 @login_required
 def dashboard_view(request):
@@ -119,3 +120,26 @@ def browse_view(request):
     }
 
     return render(request, 'browse.html', context)
+
+@login_required
+def profile_view(request):
+    user = request.user
+    profile = user.profile
+
+    notes = Note.objects.filter(uploader=user).annotate(
+        upvotes=Count('vote', filter=Q(vote__vote_type=1)),
+        downvotes=Count('vote', filter=Q(vote__vote_type=0))
+    ).order_by('-created_at')
+
+    latest_note = notes.first()
+    inferred_department = latest_note.subject.department.name if latest_note else "NotesBridge User"
+    inferred_semester = latest_note.semester if latest_note else "Current Term"
+
+    context = {
+        'profile': profile,
+        'notes': notes,
+        'inferred_department': inferred_department,
+        'inferred_semester': inferred_semester,
+    }
+
+    return render(request,'profiles/profile.html',context)
