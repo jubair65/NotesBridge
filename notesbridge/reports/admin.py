a5ead4d2615fb694d0dd76b4fbc75b9aa65e4087
AhmedLabib21 <labibahmed006@gmail.com>
2026-05-01 00:22:38 +0600

    add reports admin model

--- notesbridge/reports/admin.py
+++ notesbridge/reports/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from reports.models import Report
+
 
 # Register your models here.
+@admin.register(Report)
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('user','note','reason','created_at')
+    list_filter = ('reason',)
+    search_fields = ('details',)
